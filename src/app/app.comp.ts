957b2e36a92d9a7e201101ce65d551f130466f9f
prakashshinde <shindepy@gmail.com>
2018-08-16 08:56:04 +0530

    First

---
+++ src/app/app.comp.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'angular-app',
+    template: `
+        <div class="container">
+         This is my first angular assignment
+        </div>
+    ` 
+})
+export class AppNewComponent { }
---
+++ src/app/app.mod.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppNewComponent } from './app.comp';
+
+@NgModule({
+  imports: [
+    BrowserModule
+  ],
+  declarations: [
+    AppNewComponent
+  ],
+  bootstrap: [AppNewComponent]
+})
+export class AppModule { }
